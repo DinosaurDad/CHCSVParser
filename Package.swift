1e82bd0861a963aaf973b7e6fe0bbfa205c9b167
DinosaurDad <523433+DinosaurDad@users.noreply.github.com>
2021-11-22 16:50:27 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,17 +12,8 @@ let package = Package(
         .target(
             name: "CHCSVParser",
             dependencies: [],
-            path: ".",
-            exclude: ["README.markdown",
-                      "CHCSVParser.podspec",
-                      "LICENSE.txt",
-                      "CHCSVParser/main.m",
-                      "Unit Tests",
-                      "CHCSVParser/CHCSVParser/CHCSVParserOptions.swift",
-                     ],
-            sources: ["CHCSVParser/CHCSVParser/CHCSVParser.h",
-                      "CHCSVParser/CHCSVParser/CHCSVParser.m"],
-            publicHeadersPath: "include"
+            path: "CHCSVParser/CHCSVParser",
+            exclude: ["CHCSVParserOptions.swift"],
         )
     ]
 )
